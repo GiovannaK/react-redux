472d09cb51e93bb2bc2108bfb25b09843ac6b4e2
Giovanna Cunha <giovannakelli@gmail.com>
2021-09-16 22:04:16 -0300

    combineReducers was added

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { combineReducers, createStore } from 'redux';
+import carsReducer from './cars';
+
+const rootReducer = combineReducers({
+  cars: carsReducer
+})
+
+export default createStore(rootReducer);
