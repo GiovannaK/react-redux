85549dade08ba4475aeb5559247c19163c9d3f2c
Giovanna Cunha <giovannakelli@gmail.com>
2021-09-17 20:36:49 -0300

    redux toolkit was added to simplify action types and action creators

--- src/store/layout/index.js
+++ src/store/layout/index.js
@@ -1,37 +1,14 @@
+import {createAction, createReducer} from '@reduxjs/toolkit';
+
 const INITIAL_STATE = {
   showMessage: false
 }
+export const showMessage = createAction('SHOW_MESSAGE')
+export const hideMessage = createAction('HIDE_MESSAGE')
 
-export default (state = INITIAL_STATE, action) => {
-  switch(action.type){
-    case Types.SHOW_MESSAGE:
-      return {...state, showMessage: true};
-    case Types.HIDE_MESSAGE:
-      return {...state, showMessage: false};
-    default:
-      return state;
-  }
-}
+export default createReducer(INITIAL_STATE, {
+  [showMessage.type]: (state) => ({...state, showMessage: true}),
+  [hideMessage.type]: (state) => ({...state, showMessage: false})
+})
 
-//action types
-
-export const Types = {
-  SHOW_MESSAGE: 'SHOW_MESSAGE',
-  HIDE_MESSAGE: 'HIDE_MESSAGE'
-}
-
-//action creators
-
-export const Creators = {
-  showMessage: () => {
-    return {
-      type: Types.SHOW_MESSAGE
-    }
-  },
-  hideMessage: () => {
-    return {
-      type: Types.HIDE_MESSAGE
-    }
-  }
-};
 
