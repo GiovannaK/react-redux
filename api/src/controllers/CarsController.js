c0430616dee277cae3bc0ce23a9d11454c00ed7a
Giovanna Cunha <giovannakelli@gmail.com>
2021-09-20 00:00:39 -0300

    create method was added

--- api/src/controllers/CarsController.js
+++ api/src/controllers/CarsController.js
@@ -1,10 +1,19 @@
-class CarsController {
+const Car = require('../models/Cars');
 
-  index(req, res) {
-    return res.json({
-      message: 'test'
-    })
+class CarsController {
+  constructor(dao){
+    this.dao = dao
+  }
+  index =  async (req, res) => {
+    const cars = await this.dao.find();
+    return res.json(cars);
+  }
+
+  create =  async (req, res) => {
+    const car = await this.dao.create(req.body)
+
+    return res.status(201).json(car)
   }
 }
 
-module.exports = new CarsController();
+module.exports = new CarsController(Car);
--- api/src/routes.js
+++ api/src/routes.js
@@ -4,6 +4,7 @@ const CarsController = require('./controllers/CarsController');
 const router = Router()
 
 router.get('/cars', CarsController.index)
+router.post('/cars', CarsController.create)
 
 
 module.exports = router;
